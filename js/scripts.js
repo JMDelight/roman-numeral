266c63896c5e9d8b7ea1f08c8ca3f079ed5f8470
Alan Denison and Joel Delight <alan@>
2016-06-09 10:14:09 -0700

    finish our app and style it some

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-var numerals = ["M", "C", "X", "I", "D", "L", "V"]
-var numbers = ["1000", "500", "100", "50", "10", "5", "1"]
-
-
-// var digitConveror
+var numerals = ["M", "C", "X", "I", "D", "L", "V"];
 
 var convertor = function(number) {
   var num = number + 10000;
@@ -12,19 +8,29 @@ var convertor = function(number) {
   for(i=1; i<=4; i++) {
     var workingNumber = parseInt(numberArray[i]);
     if (workingNumber === 9) {
-      result = result + numerals[i-1] + numerals[i-2];
+      result = result + numerals[i - 1] + numerals[i - 2];
       workingNumber-= 9;
     } else if (workingNumber === 4) {
-      result = result + numerals[i-1] + numerals[i+2];
-      workingNumber -=4;
-
-    }
-    else {
-      for (index = workingNumber; index > 0; index -- ) {
-        result = result + numerals[i-1];
+      result = result + numerals[i - 1] + numerals[i + 2];
+      workingNumber -= 4;
+    } else {
+      while (workingNumber >= 5) {
+        result = result + numerals[i + 2];
+        workingNumber -= 5;
       };
-  }
-  }
-
+      for (index = workingNumber; index > 0; index -- ) {
+        result = result + numerals[i - 1];
+      };
+    }
+  };
   return result;
 };
+
+$(document).ready(function() {
+  $(".roman-form").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("#user-input").val());
+    var output = convertor(userInput);
+    $("#result").text(output);
+  });
+});
